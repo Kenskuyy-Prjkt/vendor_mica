0b44d83b472a93809b318470c0980f4adb1ef132
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    common: Switch to AOSPA's face unlock implementation

--- config/common.mk
+++ config/common.mk
@@ -113,6 +113,19 @@ PRODUCT_PACKAGES += \
     ColumbusService
 endif
 
+# FaceUnlock
+ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
+
 -include vendor/mica-priv/keys/keys.mk
 $(call inherit-product-if-exists, vendor/certification/config.mk)
 
